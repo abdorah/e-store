06db20921516391fef82fac74df8e26de68aa58b
johnrepogit <john.repo.git@gmail.com>
2022-09-13 01:01:42 -0400

    jwt _

--- src/main/java/com/eccom/store/repository/RoleRepository.java
+++ src/main/java/com/eccom/store/repository/RoleRepository.java
@@ -3,9 +3,10 @@ package com.eccom.store.repository;
 import com.eccom.store.model.Item;
 import com.eccom.store.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends CrudRepository<Role, String> {
 
 }
